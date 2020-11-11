5266dc8e870226d6f5dbaf18fac84f6094fa3d27
Arnold <40414978+PatriceJiang@users.noreply.github.com>
2020-11-11 17:18:25 +0800

    update shader

--- cocos/core/3d/builtin/init.ts
+++ cocos/core/3d/builtin/init.ts
@@ -288,7 +288,7 @@ class BuiltinResMgr {
         // ui spine two color material
         const spineTwoColorMtl = new legacyCC.Material();
         spineTwoColorMtl._uuid = 'ui-spine-two-colored-material';
-        spineTwoColorMtl.initialize({ defines: { USE_TEXTURE: true, CC_USE_EMBEDDED_ALPHA: true, IS_GRAY: false }, effectName: 'builtin-spine-two-colored' });
+        spineTwoColorMtl.initialize({ defines: { USE_TEXTURE: true, CC_USE_EMBEDDED_ALPHA: true, IS_GRAY: false }, effectName: 'spine-two-colored' });
         resources[spineTwoColorMtl._uuid] = spineTwoColorMtl;
     }
 }
