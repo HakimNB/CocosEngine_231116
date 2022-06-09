305c3393820da4fa834126dbfc9a3302aac3b728
PatriceJiang <397136899@qq.com>
2022-06-09 17:26:56 +0800

    update logs

--- native/cmake/scripts/plugins_parser.js
+++ native/cmake/scripts/plugins_parser.js
@@ -47,7 +47,7 @@ function search_cc_config_json_levels(dir, depth) {
 
 for (let searchPath of PROJ_SEARCH_PATHS) {
     if (!fs.existsSync(searchPath)) {
-        console.error(`[searching plugins] directory ${searchPath} does not exist`);
+        console.warn(`[searching plugins] directory ${searchPath} does not exist`);
         continue;
     }
     search_cc_config_json_levels(searchPath, 1);
@@ -105,6 +105,10 @@ function find_files_in_dir_recursive(depth, possible_names, dir, results) {
     if (depth > MAX_SEARCH_LEVEL) {
         return;
     }
+    if(!fs.existsSync(dir)) {
+        console.warn(`  [error] '${dir}' does not exist`)
+        return;
+    }
     let list = fs.readdirSync(dir);
     let dstFiles = possible_names.map(x => path.join(dir, x)).filter(f => {
         return fs.existsSync(f);
@@ -132,13 +136,13 @@ function search_config_for_cmake(libs, dir) {
     let config_directories = [];
     for (let lib of libs) {
         const cmake_dirs = [];
-        const raw_lib_name = lib.split(/:|@/)[0];
+        const raw_lib_name = lib.split(/@/)[0];
         const possible_names = [`${raw_lib_name}-config.cmake`, `${raw_lib_name}Config.cmake`];
         find_files_in_dir_recursive(0, possible_names, dir, cmake_dirs);
         const cfgDirs = cmake_dirs.map(x => path.resolve(path.dirname(x)));
         if (cmake_dirs.length > 1) {
-            console.error(`multiple ${raw_lib_name}-config.cmake found`);
-            console.error(cmake_dirs);
+            console.warn(`multiple ${raw_lib_name}-config.cmake found`);
+            console.warn(cmake_dirs);
         }
         if (cmake_dirs.length > 0) {
             config_directories.push({ name: `${raw_lib_name}_DIR`, path: cfgDirs[0] });
@@ -217,6 +221,8 @@ function validate_cc_plugin_json_format(tag, content) {
 
 console.log(`Engine version: ${read_engine_version()}`);
 
+/// Generate Pre-AutoLoadPlugins.cmake
+
 let output_lines = ["# plugins found & enabled in search path", "", ""];
 for (let plugin_dir of cc_config_json_list) {
     let load_plugins = [];
@@ -247,15 +253,13 @@ for (let plugin_dir of cc_config_json_list) {
                     output_lines.push(`set(${cfg.name} "\${CC_PROJECT_DIR}/${relocate}")`);
                 }
             }
-            // console.log(pkg);
-            // output_lines.push(`cc_load_plugin(TARGET ${pkg.target} ${pkg.depends.length > 0 ? "DEPENDS " + pkg.depends.map(x => `"${x}"`).join(" ") : ""} PATHS "${project_to_plugin_dir}/${PLATFORM_NAME_FROM_CMAKE}")`)
             load_plugins = load_plugins.concat([...pkg.depends, pkg.target]);
             output_lines.push(`list(APPEND CC_LOADED_PLUGINS`);
             output_lines = output_lines.concat(`  ${pkg.target}`);
             output_lines.push(`)`);
         }
         let search_path = `${project_to_plugin_dir}/${PLATFORM_NAME_FROM_CMAKE}`;
-        let plugin_names = load_plugins.map(x => x.split(/:|@/));
+        let plugin_names = load_plugins.map(x => x.split(/@/));
         for (let plg of plugin_names) {
             output_lines.push("");
             if (plg[1]) {
@@ -265,6 +269,7 @@ for (let plugin_dir of cc_config_json_list) {
             }
             output_lines.push(`  NAMES "${plg[0]}"`);
             output_lines.push(`  PATHS "${search_path}"`);
+            output_lines.push(`  NO_DEFAULT_PATH`);
             output_lines.push(`)`);
         }
         if (packages.length > 0) {
